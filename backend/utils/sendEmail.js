3043f25b981c35d7d392c3e596ee05edf936ce7f
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2025-11-05 13:48:22 +0530

    Correct comments

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,8 +4,8 @@ export const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     //service: "gmail",
     host: "smtp.gmail.com", // explicit instead of "service"
-    port: 587, // secure SSL port
-    secure: false, // use SSL
+    port: 587, // STARTTLS port
+    secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.NODEMAILER_EMAIL,
       pass: process.env.NODEMAILER_PASS,
