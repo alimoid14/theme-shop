070659910711d8a869d4c77339736026e365396d
alimoid14 <syedalimoidrizvi@gmail.com>
2025-12-10 12:34:14 +0530

    Add trycatch to send email func

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,11 +1,10 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    //service: "gmail",
-    host: "smtp.gmail.com", // explicit instead of "service"
-    port: 587, // STARTTLS port
-    secure: false, // true for 465, false for other ports
+  try{
+    const transporter = nodemailer.createTransport({
+    service: "gmail",
+    
     auth: {
       user: process.env.NODEMAILER_EMAIL,
       pass: process.env.NODEMAILER_PASS,
@@ -18,4 +17,7 @@ export const sendEmail = async (to, subject, text) => {
     subject,
     text,
   });
+}catch(error){
+  console.log(error);
+}
 };
